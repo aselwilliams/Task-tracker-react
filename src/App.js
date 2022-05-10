ab45d401b82ee7fc5563c26b53faa5a3f73ce59d
aselwilliams <aselwilliams22@gmail.com>
2022-05-09 20:53:24 -0400

    Add button enabled

--- src/App.js
+++ src/App.js
@@ -26,6 +26,13 @@ function App() {
         reminder:false,
     },
     ])
+
+    //Add Task
+    const addTask=(task)=>{
+    const id=Math.floor(Math.random()*10000)+1
+    const newTask={id,...task}
+    setTasks([...tasks, newTask])
+    }
     //Delete Task
     const deleteTask=(id)=>{
       setTasks(tasks.filter((task)=>task.id!==id))
@@ -38,7 +45,7 @@ function App() {
   return (
     <div className="container">
      <Header />
-     <AddTask />
+     <AddTask onAdd={addTask} />
      {tasks.length > 0 ? (<Tasks tasks={tasks} onDelete={deleteTask} onToggle={toggleReminder} />) : ( ' No Tasks To Show')}
     </div>
   );
