eda53db77f29d2f47296063afcb6558d1d7d70a2
aselwilliams <aselwilliams22@gmail.com>
2022-05-09 21:09:36 -0400

    toggle add or close btn functionality added

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,14 +1,12 @@
 import PropTypes from 'prop-types';
 import Button from './Button';
 
-const Header = ({title}) => {
-    const onClick=()=>{
-        console.log('click')
-    }
+const Header = ({title, onAdd, showAdd}) => {
+  
   return (
     <header className='header'>
         <h1>{title}</h1>
-        <Button onClick={onClick} color='green' text='Add' />
+        <Button onClick={onAdd} color={showAdd ? 'brown' : 'green'} text={showAdd ? 'Close' : 'Add'} />
     </header>
   )
 }
